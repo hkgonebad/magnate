f343b0d4d3fb29e5f7962f9aaeb1bc3e8454a39f
awzdigital <theawzdigital@gmail.com>
2023-08-17 12:54:34 +0530

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,42 @@
+///////////////////////////////////
+$(function() {
+
+    // Header
+    const headerUrl = "header.html";
+    $("#header").load(headerUrl, function() {
+
+        // Active
+        const currentUrl = window.location.href;
+        $(".navbar-nav .nav-link, .navbar-nav .dropdown-item").each(function () {
+            if (this.href === currentUrl) {
+                $(this).addClass("active");
+            }
+        });
+    })
+
+    // Work Together CTA
+    const wtCtaUrl = "work-together.html";
+    $("#workTogether").load(wtCtaUrl, function() {
+        
+    });
+
+    // Footer
+    const footerUrl = "footer.html";
+    $("#footer").load(footerUrl, function() {
+
+    });
+
+    // Main Slider
+    if ($('.mainSlider').length > 0) {
+
+        $('.mainSlider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+            autoplay: true,
+            autoplayDuration: 3000,
+            arrows: false,
+        });
+    }
+
+})
